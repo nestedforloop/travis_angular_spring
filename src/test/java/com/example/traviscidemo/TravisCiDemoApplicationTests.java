dfc47729c7dd9fcb3e8a36ba5068ab3762ca213a
nestedforloop <nestedforloop@users.noreply.github.com>
2019-11-05 20:31:34 +0100

    add spring initializr project and travis yaml file

---
+++ src/test/java/com/example/traviscidemo/TravisCiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.traviscidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisCiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
