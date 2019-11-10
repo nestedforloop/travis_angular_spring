bfca869b873d7345379490aed816ae69ade729b6
nestedforloop <nestedforloop@users.noreply.github.com>
2019-11-10 20:10:24 +0100

    edit directory structure & yml

--- src/main/java/com/example/traviscidemo/service/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.traviscidemo.service;
-
-import com.example.traviscidemo.model.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Long> {
-}
